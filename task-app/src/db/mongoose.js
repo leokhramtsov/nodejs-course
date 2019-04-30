3ce81d4a2c99965a674682c54921e9ad7662cdba
unknown <hediirix@gmail.com>
2019-04-29 22:18:31 -0400

    Lesson: task-app - create/read/update routes

--- task-app/src/db/mongoose.js
+++ task-app/src/db/mongoose.js
@@ -2,5 +2,6 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-app', {
   useNewUrlParser: true,
-  useCreateIndex: true
+  useCreateIndex: true,
+  useFindAndModify: false
 });
--- task-app/src/index.js
+++ task-app/src/index.js
@@ -12,28 +12,131 @@ app.get('/', (req, res) => {
   res.send('root');
 });
 
-app.post('/users', (req, res) => {
-  const { name, email, password } = req.body;
-
-  const user = new User({ name, email, password });
-  user
-    .save()
-    .then(user => {
-      res.status(201).json(user);
-    })
-    .catch(err => res.status(400).send(err));
+app.post('/users', async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.status(201).json(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
 
-app.post('/tasks', (req, res) => {
-  const { description } = req.body;
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json(users);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
 
-  const task = new Task({ description });
-  task
-    .save()
-    .then(task => {
-      res.status(201).json(task);
-    })
-    .catch(err => res.status(400).send(err));
+app.get('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findById({ _id: req.params.id });
+
+    if (!user) {
+      return res.status(404).json({ message: 'No user found' });
+    }
+
+    res.status(200).json(user);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+app.patch('/users/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['name', 'password'];
+  const isAllowedUpdate = updates.every(update =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isAllowedUpdate) {
+    return res
+      .status(400)
+      .json({ message: 'Update request contains invalid field.' });
+  }
+
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: 'No user found.' });
+    }
+
+    res.status(200).json(user);
+  } catch (e) {
+    res.status(400).send();
+  }
+});
+
+app.post('/tasks', async (req, res) => {
+  const task = new Task(req.body);
+  try {
+    await task.save();
+    res.status(201).json(task);
+  } catch (e) {
+    res.status(400).send();
+  }
+});
+
+app.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json(tasks);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+app.get('/tasks/:id', async (req, res) => {
+  try {
+    const task = await Task.findById({ _id: req.params.id });
+
+    if (!task) {
+      return res.status(404).json({ message: 'No task found' });
+    }
+
+    res.status(200).json(task);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+app.patch('/tasks/:id', async (req, res) => {
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!task) {
+      return res.status(404).json({ message: 'No task found.' });
+    }
+
+    res.status(200).json(task);
+  } catch (e) {
+    res.status(400).send();
+  }
+});
+
+// Playground
+app.delete('/tasks/:id', async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) {
+      return res.status(404).json({ message: 'Task not found.' });
+    }
+
+    const count = await Task.countDocuments({ completed: false });
+    res.status(200).json(count);
+  } catch (e) {
+    res.status(500).json(e);
+  }
 });
 
 app.listen(port, () => console.log(`Server is up on port ${port}`));
